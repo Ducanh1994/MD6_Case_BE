2981a991152dbb3007c457a77a7bba8f569f0ddc
quocm <quocminhhoang5004@gmail.com>
2023-06-14 09:53:35 +0700

    Added temporary router for admin

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,12 @@
 import {Router} from "express";
 import {userRouter} from "./userRouter";
 import {productRouter} from "./productRouter";
+import {adminRouter} from "./adminRouter";
 
 const router = Router();
 
 router.use('/auth', userRouter);
+// Admin Router is temporary. Please refactor for cleaner code
+router.use('/auth/admin', adminRouter)
 router.use('/products', productRouter);
 export default router;
