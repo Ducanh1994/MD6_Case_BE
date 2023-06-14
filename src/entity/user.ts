b54884cbd17c3611f6d0a09f2c281ff43bcde353
quocm <quocminhhoang5004@gmail.com>
2023-06-14 09:50:30 +0700

    Fix folder name and added extra boundary for entity

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,30 +1,32 @@
 import {Column, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn} from 'typeorm';
 import {Order} from "./order";
 import {Store} from "./store";
+
 @Entity()
+//Some attributes can't be empty and can only exist once. Use Unique and Nullable
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
-    @Column({unique:true})
+    @Column({type:"varchar", unique:true, nullable: false})
     username: string;
-    @Column({unique:true})
+    @Column({type:"varchar", unique:true, nullable:false})
     email: string;
-    @Column()
+    @Column({type:"varchar", default:'123456@Abc', nullable:false})
     password: string;
-    @Column({default:'user'})
+    @Column({type:"varchar", default:'user', nullable: false})
     role: string;
-    @Column()
+    @Column({type:"varchar", nullable:false})
     name: string;
-    @Column()
+    @Column({type:"varchar", nullable:false})
     age: number;
-    @Column()
+    @Column({type: "varchar",nullable:true})
     phoneNumber: number;
-    @Column()
+    @Column({type:"varchar", nullable:true})
     address: string;
-    @Column()
+    @Column({type:"varchar", nullable: true})
     salary: number;
     @OneToMany(() => Order,(order) => order.user)
     orders: Order[];
     @OneToOne(() => Store,(store) => store.user)
     store: Store;
- }
+}
