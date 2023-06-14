aa9022770f9fe22d766c4b3d55f849e4e77d5ccb
quocm <quocminhhoang5004@gmail.com>
2023-06-14 09:51:16 +0700

    Added Admin Router

---
+++ src/router/adminRouter.ts
@@ -0,0 +1,7 @@
+import {Router} from 'express'
+import AdminController from "../controller/adminController";
+
+export const adminRouter = Router()
+
+//Router for Admin Specific Function. Modified to add more feature as more sprint progress
+adminRouter.post('/createAccount', AdminController.createAccount);
