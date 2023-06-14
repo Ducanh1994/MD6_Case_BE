2ac4a09c492c99a730d58990fa385072a484f3fd
quocm <quocminhhoang5004@gmail.com>
2023-06-14 10:38:58 +0700

    Added Search Path

--- src/router/adminRouter.ts
+++ src/router/adminRouter.ts
@@ -5,4 +5,5 @@ export const adminRouter = Router()
 
 //Router for Admin Specific Function. Modified to add more feature as more sprint progress
 adminRouter.post('/createAccount/', AdminController.createAccount);
+// Search Function need to be configured with name and username query in FE
 adminRouter.get('/searchAccount/', AdminController.searchAccount);
