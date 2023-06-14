e15f6a7e3c6101a2f8be14cf496fa9355e038de0
quocm <quocminhhoang5004@gmail.com>
2023-06-14 11:19:11 +0700

    Improved Syntax

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -5,24 +5,24 @@ class AdminController {
     private adminService;
 
     constructor() {
-        this.adminService = adminService
+        this.adminService = adminService;
     }
 
     // Await Update For Creating New Account
     createAccount = async (req: Request, res: Response) => {
         let userCheck = await adminService.checkUser(req.body);
         if (userCheck.length !== 0) {
-            await res.status(400).json('The username already existed!')
+            await res.status(406).json('The username already existed!');
         } else {
             await adminService.createUser(req.body);
-            await res.status(201).json('Create New User Successfully!');
+            await res.status(201).json('Create new user successfully!');
         }
     }
 
     // Await Response For Found Entity
     searchAccount = async (req: Request, res: Response) => {
-        let userFound = await adminService.searchUser(req.query)
-        await res.status(201).json(userFound)
+        let userFound = await adminService.searchUser(req.query);
+        await res.status(201).json(userFound);
     }
 }
 
