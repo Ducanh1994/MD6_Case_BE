a9c4bb2ca12dc12f8b7a07ed32e7bd642ca3ffc6
quocm <quocminhhoang5004@gmail.com>
2023-06-14 09:54:27 +0700

    Added Admin Service

---
+++ src/service/adminService.ts
@@ -0,0 +1,35 @@
+import {User} from "../entity/user";
+import {AppDataSource} from "../data-source";
+import bcrypt from 'bcrypt'
+
+class AdminService {
+    private userRepository;
+
+    constructor() {
+        this.userRepository = AppDataSource.getRepository(User);
+    }
+
+    // Conditional To Create New Account
+    checkUser = async (user) => {
+        let findName = await this.userRepository.find({
+            where: [
+                {
+                    username: user.username
+                },
+                {
+                    name: user.name
+                }
+            ]
+        })
+        return findName;
+    }
+
+    // Admin Create New Account
+    createUser = async (user) => {
+        user.password = await bcrypt.hash(user.password,10);
+        return this.userRepository.save(user);
+    }
+}
+
+export default new AdminService();
+
