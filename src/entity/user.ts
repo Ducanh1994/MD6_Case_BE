a63c041f130135256c51d7ad038ff6fa291e840f
QMinh102 <120642136+QMinh102@users.noreply.github.com>
2023-06-14 15:41:47 +0700

    Update Comment and Entity Boundary

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -3,27 +3,27 @@ import {Order} from "./order";
 import {Store} from "./store";
 
 @Entity()
-//Some attributes can't be empty and can only exist once. Use Unique and Nullable
+//Some attributes can be empty and can only exist once. Use Unique and Nullable
 export class User {
     @PrimaryGeneratedColumn()
     id: number;
-    @Column({type:"varchar", unique:true, nullable: false})
+    @Column({type: "varchar", unique:true})
     username: string;
-    @Column({type:"varchar", unique:true, nullable:false})
+    @Column({type: "varchar", unique:true})
     email: string;
-    @Column({type:"varchar", default:'123456@Abc', nullable:false})
+    @Column({type: "varchar", default:'123456@Abc'})
     password: string;
-    @Column({type:"varchar", default:'user', nullable: false})
+    @Column({type: "varchar", default:'user'})
     role: string;
-    @Column({type:"varchar", nullable:false})
+    @Column({type: "varchar"})
     name: string;
-    @Column({type:"varchar", nullable:false})
+    @Column({type: "varchar"})
     age: number;
     @Column({type: "varchar",nullable:true})
     phoneNumber: number;
-    @Column({type:"varchar", nullable:true})
+    @Column({type: "varchar", nullable:true})
     address: string;
-    @Column({type:"varchar", nullable: true})
+    @Column({type: "varchar", nullable: true})
     salary: number;
     @OneToMany(() => Order,(order) => order.user)
     orders: Order[];
