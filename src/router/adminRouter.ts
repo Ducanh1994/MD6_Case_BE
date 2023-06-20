4e8fe898c6396f029f1d4ba490bfd92a539dd74b
Ducanh1994 <121839766+Ducanh1994@users.noreply.github.com>
2023-06-20 17:35:19 +0700

    commit adminSearchAccount

--- src/router/adminRouter.ts
+++ src/router/adminRouter.ts
@@ -7,10 +7,8 @@ export const adminRouter = Router()
 
 adminRouter.use(auth)
 adminRouter.use(adminAuth)
-//Router for Admin Specific Function. Modified to add more feature as more sprint progress
 adminRouter.post('/createAccount/', AdminController.createAccount);
 adminRouter.get('/showAccount/', AdminController.showAllAccount);
-// Search Function need to be configured with name and username query in FE
 adminRouter.get('/searchAccount/', AdminController.searchAccount);
 adminRouter.post('/enablingAccount/', AdminController.enablingShopAccount);
 
